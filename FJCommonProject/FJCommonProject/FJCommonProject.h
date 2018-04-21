1a3884ee62a2e42237d46521bcccbb6d55e2feea
熊伟 <597370561@qq.com>
2018-04-21 11:20:39 +0800

    【Modify】【Bearger】add project

---
+++ FJCommonProject/FJCommonProject/FJCommonProject.h
@@ -0,0 +1,19 @@
+//
+//  FJCommonProject.h
+//  FJCommonProject
+//
+//  Created by 熊伟 on 2018/4/21.
+//  Copyright © 2018年 熊伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FJCommonProject.
+FOUNDATION_EXPORT double FJCommonProjectVersionNumber;
+
+//! Project version string for FJCommonProject.
+FOUNDATION_EXPORT const unsigned char FJCommonProjectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FJCommonProject/PublicHeader.h>
+
+
